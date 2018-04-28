85268245b5efc3bc8b9ceebada65ec24616bd5e4
jbolt256 <jbolt256@gmail.com>
2018-04-28 15:58:06 -0400

    improve quality of svg files

--- source/compiler/svg.d
+++ source/compiler/svg.d
@@ -5,24 +5,26 @@ import std.stdio, std.string, std.conv, std.format, std.range;
 class SVGW {
 	public int line;
 	
+	/** 
+	 * Writes to program.svg
+	 */
 	public void write(int opcode, int databits, int auxbits) {
-		int[int] polyfill;
 		int i = 0;
 		string textOut = "";
 		string basicText = "";
 		
+		/* Convert the three bit sections to binary numbers formatted to a desired length (6, 20, 6 bits) then merge them to one 32-bit string */
 		string opcodeBin = format!"%06b"(opcode);
 		string databitsBin = format!"%020b"(databits);
 		string auxbitsBin = format!"%06b"(auxbits);	
 		string allBits = opcodeBin ~ databitsBin ~ auxbitsBin;
 		
+		/* Split by the sharp3 id, then add it again, later on merge with new text */
 		if ( std.file.exists("program.svg") ) {
 			basicText = to!string(std.file.read("program.svg")).split("<g id=\"sharp3\" transform=\"translate(0.5,0.5)\">")[0] ~ "<g id=\"sharp3\" transform=\"translate(0.5,0.5)\">";
-			//foreach( sLine; File("program.svg").byLine().take(1)) {
-			//	basicText ~= sLine;
-			//}
 		}
 		
+		/* Write black boxes wherever zeroes are located in allBits string */
 		foreach ( bit; allBits.stride(1)) { 
 			if ( bit == '0' ) {
 				textOut ~= format("<rect x=\"%s\" y=\"%s\" width=\"14\" height=\"5\" />", 14*i, 5 * this.line);
@@ -33,10 +35,16 @@ class SVGW {
 		std.file.write("program.svg", basicText ~ textOut);
 	}
 	
+	/** 
+	 * Closes SVG file by appending last two necessary tags.
+	 */
 	public void close() {
 		std.file.append("program.svg", "</g></svg>");
 	}
 	
+	/** 
+	 * Opens SVG file by placing a newline before where the new text will be added.
+	 */
 	public void open() {
 		std.file.append("program.svg", "\r\n");
 	}
